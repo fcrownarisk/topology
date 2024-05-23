4941968d706fa0dd0b43c8de9dd3f8cef74e14c6
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-23 08:31:57 +0800

    Update prism2.ts

--- VerticeEdges/Vertice/prism2.ts
+++ VerticeEdges/Vertice/prism2.ts
@@ -1,4 +1,3 @@
-import 'topology/VEF/prism'
 interface Prism {
     length: number
     width: number
@@ -10,9 +9,9 @@ void function SurfaceArea(Prism:number){
 void function Volume(Prism: number) {
     return [length * this.width * this.height]
 }
-const dimensions = {
+const dimension = {
     length: 1,
     width: Math.sqrt(3)/2,
     height: 1
 }
-console.log('Prism','SurfaceArea','volume',dimensions)
+console.log('SurfaceArea','volume',dimension)
