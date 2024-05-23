387c346579750e714c08085cbd2f27eed256ee77
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-23 08:35:42 +0800

    Update cylinder.ts

--- VolumnSurface/cylinder.ts
+++ VolumnSurface/cylinder.ts
@@ -1,5 +1,5 @@
 void function Cylinder(radius: number, height: number){
-    const volume = Math.PI * (radius * radius) * height;
-    const surfaceArea = 2 * Math.PI * radius * height + 2 * Math.PI * (radius * radius);
+    const Volume = Math.PI * (radius * radius) * height;
+    const SurfaceArea = 2 * Math.PI * radius * height + 2 * Math.PI * (radius * radius);
 }
 console.log(`Volume`,`SurfaceArea`)
