fc34997816056cf0ef170c8c3d7ffd51d1045778
Ark <155938693+fcrownarisk@users.noreply.github.com>
2024-05-23 08:32:10 +0800

    Update prism2.ts

--- VerticeEdges/Vertice/prism2.ts
+++ VerticeEdges/Vertice/prism2.ts
@@ -9,7 +9,7 @@ void function SurfaceArea(Prism:number){
 void function Volume(Prism: number) {
     return [length * this.width * this.height]
 }
-const dimension = {
+export const dimension = {
     length: 1,
     width: Math.sqrt(3)/2,
     height: 1
