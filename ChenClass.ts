9fd98a78922dacefbfd8da1a64e3670fecf02b64
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-26 15:55:29 +0800

    Update ChenClass.ts

--- ChenClass.ts
+++ ChenClass.ts
@@ -1,3 +1,15 @@
 import './VerticeEdges/Vertice'
 import './VerticeEdges/Edges'
-
+export function Chen(Vertice){
+  this.hedron1 = new [Dodecaherdron2 + Icosahedron2 + Octaahedron2 + tetrahedron2]
+  this.hedron2 = new [Dodecaherdron2 - Icosahedron2 - Octaahedron2 - tetrahedron2]
+  this.hedron3 = new [Dodecaherdron2 * Icosahedron2 * Octaahedron2 * tetrahedron2]
+  this.hedron4 = new [Dodecaherdron2 % Icosahedron2 % Octaahedron2 % tetrahedron2]
+  this.hedron5 = new [hedron1 / hedron2 / hedron3 / hedron4]
+  return hedron
+}
+export function Class(Edges){
+  this.cube2 - this.cone2 - this.Pyramid2 = this.sphere2
+  this.sphere2 = this.cylinder2 - this.lightcone2 - this.prism2
+  return sphere2
+}
