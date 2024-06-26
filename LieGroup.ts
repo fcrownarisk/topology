c44c127fb06bd66e5b90b50fbf921c23a5e7bfc2
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-06-26 16:05:47 +0800

    Update LieGroup.ts

--- LieGroup.ts
+++ LieGroup.ts
@@ -1,7 +1,10 @@
 import './VolumnSurface'
 export async function Volumn(){
-   this.Dodecahedro = this.Icosahedron 
+   this.prism = this.tetrahedron + this.Dodecahdedron + this.Icosahedron
+   this.Pyramid = this.Octahedron - this.Hexahedron
+   this.lightcone - this.cone = this.sphere - this.prism
+   return super prism
 }
 export await function Surface(){
-  
+  this.
 }
