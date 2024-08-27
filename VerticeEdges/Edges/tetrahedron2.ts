921beebdeb2cff6c20b22af4c1112a365da697b9
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-28 00:29:13 +0800

    Update tetrahedron2.ts

--- VerticeEdges/Edges/tetrahedron2.ts
+++ VerticeEdges/Edges/tetrahedron2.ts
@@ -22,10 +22,8 @@ export const faces: Point[][] = [
     [vertices[0], vertices[1], vertices[2]],
     [vertices[0], vertices[2], vertices[3]],
     [vertices[0], vertices[3], vertices[4]],
-    [vertices[0], vertices[4], vertices[5]],
-    [vertices[3], vertices[1], vertices[2]],
-    [vertices[3], vertices[2], vertices[3]],
-    [vertices[3], vertices[3], vertices[4]],
-    [vertices[3], vertices[1], vertices[4]],
+    [vertices[1], vertices[2], vertices[4]],
+    [vertices[1], vertices[3], vertices[4]],
+    [vertices[2], vertices[3], vertices[4]],
 ]
 console.log(vertices,faces)
