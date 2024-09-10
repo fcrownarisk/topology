6d88b4cd0141555d1645cf9884c2f34c2fe0ad14
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-10 17:23:37 +0800

    Delete VolumnSurface directory

--- VolumnSurface/cylinder.ts
+++
@@ -1,5 +0,0 @@
-void function Cylinder(radius: number, height: number){
-    const Volume = Math.PI * (radius * radius) * height;
-    const SurfaceArea = 2 * Math.PI * radius * height + 2 * Math.PI * (radius * radius);
-}
-console.log(`Volume`,`SurfaceArea`)
