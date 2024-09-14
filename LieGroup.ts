98796a165b2819eda65f19b7787c0553c81c5774
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-14 21:37:19 +0800

    Update LieGroup.ts

--- LieGroup.ts
+++ LieGroup.ts
@@ -5,10 +5,9 @@ export async function Volumn(Lie){
    this.lightcone - this.cone ; this.cylinder - this.prism
 }
 await function Surface(Group){
-  this.sphere1 = (this.Dodecahedron + this.Icosahedron + this.tetrahedron) * (this.Octahedron + this.Hexahedron)
-  this.sphere2 = this.cylinder + this.cone + this.prism + this.lightcone
-  this.sphere3 = this.cylinder - this.cone - this.prism - this.lightcone
-  this.sphere4 = this.cylinder * this.cone * this.prism * this.lightcone
-  this.sphere5 = this.cylinder / this.cone / this.prism / this.lightcone
-  this.sphere6 = this.sphere1 % this.sphere2 % this.sphere3 % this.sphere4 % this.sphere
+  this.sphere1 = this.cylinder + this.cone + this.prism + this.lightcone
+  this.sphere2 = this.cylinder - this.cone - this.prism - this.lightcone
+  this.sphere3 = this.cylinder * this.cone * this.prism * this.lightcone
+  this.sphere4 = this.cylinder / this.cone / this.prism / this.lightcone
+  this.sphere5 = this.sphere1 % this.sphere2 % this.sphere3 % this.sphere4 % this.sphere
 }
