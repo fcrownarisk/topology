e3c46d7d6e215cf8306ee07d7ea3616fd3cbdfdd
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-09-15 09:05:34 +0800

    Update ChenClass.ts

--- ChenClass.ts
+++ ChenClass.ts
@@ -4,8 +4,8 @@ export function Chen(Vertice){
   this.hedron1 = this.Dodecaherdron2 + this.Icosahedron2 + this.Octaahedron2 + this.tetrahedron2
   this.hedron2 = this.Dodecaherdron2 - this.Icosahedron2 - this.Octaahedron2 - this.tetrahedron2
   this.hedron3 = this.Dodecaherdron2 * this.Icosahedron2 * this.Octaahedron2 * this.tetrahedron2
-  this.hedron4 = this.Dodecaherdron2 % this.Icosahedron2 % this.Octaahedron2 % this.tetrahedron2
-  this.hedron5 = this.hedron1 / this.hedron2 / this.hedron3 / this.hedron4
+  this.hedron4 = this.Dodecaherdron2 / this.Icosahedron2 / this.Octaahedron2 / this.tetrahedron2
+  this.hedron5 = this.hedron1 % this.hedron2 % this.hedron3 % this.hedron4
 }
 export function Class(Edges){
   this.cube2 - this.cone2 - this.Pyramid2 ; this.sphere2
